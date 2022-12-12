184cdef5aae75c30ec9ca0da9c2584803986deb8
SeverinFux <severin.fux@lernende.bbw.ch>
2022-12-12 12:47:53 +0100

    componentsPush

--- src/pages/ZooWrapper.jsx
+++ src/pages/ZooWrapper.jsx
@@ -1,8 +1,10 @@
 import Names from "./components/names";
+import Login from "./components/login";
 
 function ZooWrapper() {
     return (
         <>
+            <Login/>
             <Names name={'Séverin'}/>
         </>
     )
