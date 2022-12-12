df986d2f080f93edf9e0c842b80e75badec039e3
SeverinFux <105064196+SeverinFux@users.noreply.github.com>
2022-12-12 12:57:04 +0100

    Severinbranch (#2)
    * some components

--- src/pages/ZooWrapper.jsx
+++ src/pages/ZooWrapper.jsx
@@ -1,6 +1,12 @@
+import Names from "./components/names";
+import Login from "./components/login";
+
 function ZooWrapper() {
     return (
-        <></>
+        <>
+            <Login/>
+            <Names name={'Séverin'}/>
+        </>
     )
 }
 
