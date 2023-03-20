3561fc8d5ac829589e0b888d6d43a1a11d1912ee
SeverinFux <severin.fux@lernende.bbw.ch>
2023-03-20 16:28:51 +0100

    testee

---
+++ src/pages/MainComponent.jsx
@@ -0,0 +1,16 @@
+function MainComponent() {
+    const list =
+        [
+            {name:"apfel"},
+            {name:"birne"},
+            {name:"banane"}
+        ]
+    return (
+        <>
+            Main
+            <footer>@Séverin 16/01/23</footer>
+        </>
+    )
+}
+
+export default MainComponent
