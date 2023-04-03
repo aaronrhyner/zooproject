316307cb2d2a1ee65dcc0b379588c9575af516b0
SeverinFux <severin.fux@lernende.bbw.ch>
2023-04-03 12:57:04 +0200

    WIP: besser?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css'
 import ZooWrapper from './pages/ZooWrapper'
-import News from './pages/news/News'
 
 function App() {
   return (
     <div className="App">
       <ZooWrapper />
-      <br />
-      <News />
     </div>
   )
 }
