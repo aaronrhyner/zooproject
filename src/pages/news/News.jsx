647d0fb80ac15a162d58daa05c125ca488c9b1fc
SeverinFux <severin.fux@lernende.bbw.ch>
2023-04-04 15:13:26 +0200

    WIP: Newspage and: * /news * /legalnotice * /privacypolicy in Router added

---
+++ src/pages/news/News.jsx
@@ -0,0 +1,61 @@
+import { Box, Grid } from '@mui/material'
+import news from './NewsArray'
+
+function News() {
+  const boxStyling = {
+    backgroundColor: 'Lightgray',
+    borderRadius: '5px',
+    width: 'calc(30% + 400px)',
+    border: '1px solid black',
+    padding: '30px 30px 30px 30px',
+    textAlign: 'left',
+    marginLeft: '10px',
+    marginBottom: '5px',
+    boxShadow: '10px 5px 15px black'
+  }
+
+  const imgStyling = {
+    border: '1px solid black',
+    padding: '0 0'
+  }
+
+  return (
+    <div style={{ backgroundColor: 'lightblue' }}>
+      <h1>News</h1>
+
+      {news.map((news) => (
+        <Box
+          style={boxStyling}
+          onClick={() => alert('Going On With New Page')}
+          on
+        >
+          <Grid container spacing={3} columns={16}>
+            <Grid item xs={3}>
+              <img
+                style={imgStyling}
+                src={news.img}
+                alt={news.alt}
+                height={100}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <h3>{news.titel}</h3>
+              <h6>{news.text}</h6>
+            </Grid>
+            <Grid item xs={6}>
+              Time: <u>{news.time}</u> Uhr <br />
+              Place: {news.place}
+              <br />
+            </Grid>
+          </Grid>
+        </Box>
+      ))}
+      <br />
+      <div style={{ border: '1px dotted black', backgroundColor: 'pink' }}>
+        @Séverin
+      </div>
+    </div>
+  )
+}
+
+export default News
---
+++ src/pages/news/NewsArray.js
@@ -0,0 +1,27 @@
+const news = [
+  {
+    titel: 'Fledermaus Geburt ',
+    img: '/pictures/news/bat.png',
+    alt: 'Fledermaus-Bild ',
+    text: 'Wow der echte frische Batman',
+    time: '15:15',
+    place: 'Flatterhaus Nr. 13'
+  },
+  {
+    titel: 'Helloween Special ',
+    img: '/pictures/news/helloween.png',
+    alt: 'Helloween-Bild ',
+    text: 'Kürbis Schnitzen im Löwengehege',
+    time: '16:45',
+    place: 'Löwengehege'
+  },
+  {
+    titel: 'Vogel Fütterung',
+    img: '/pictures/news/vogel.png',
+    alt: 'Helloween-Bild ',
+    text: 'Ausführliche Futterwahl und Erkärung',
+    time: '11:00',
+    place: 'Vogel-Station 4'
+  }
+]
+export default news
