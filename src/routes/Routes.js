12feaa88d8f26bff1eee133706496400c73a33d5
SeverinFux <severin.fux@lernende.bbw.ch>
2023-04-04 15:13:10 +0200

    WIP: Newspage and: * /news * /legalnotice * /privacypolicy in Router added

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,11 @@
 import Home from '../pages/home/Home'
 import HomeIcon from '@mui/icons-material/Home'
+import PolicyIcon from '@mui/icons-material/Policy'
+import GavelIcon from '@mui/icons-material/Gavel'
+import NewspaperIcon from '@mui/icons-material/Newspaper'
+import News from '../pages/news/News'
+import LegalNotice from '../pages/legalNotice/LegalNotice'
+import PrivacyPolicy from '../pages/privacyPolicy/PrivacyPolicy'
 
 const routes = [
   {
@@ -7,6 +13,24 @@ const routes = [
     name: 'Home',
     element: <Home />,
     icon: <HomeIcon />
+  },
+  {
+    path: '/news',
+    name: 'News',
+    element: <News />,
+    icon: <NewspaperIcon />
+  },
+  {
+    path: '/legalnotice',
+    name: 'Legal Notice',
+    element: <LegalNotice />,
+    icon: <GavelIcon />
+  },
+  {
+    path: '/privacypolicy',
+    name: 'Privacy Policy',
+    element: <PrivacyPolicy />,
+    icon: <PolicyIcon />
   }
 ]
 
