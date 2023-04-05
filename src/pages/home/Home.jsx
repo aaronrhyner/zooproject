1bc8b3169ee105a285f9157106d7a73508911b6f
aaronrhyner <aaron.rhyner@lernende.bbw.ch>
2023-04-05 23:06:25 +0200

    created sidebar component, added more prettier options

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,6 @@ import { Typography } from '@mui/material'
 
 export default function Home() {
   return (
-    <Typography component={'h1'}>Hey Friends That's Hour Main Page!</Typography>
+    <Typography variant={'h4'}>Hey Friends That's Hour Main Page!</Typography>
   )
 }
